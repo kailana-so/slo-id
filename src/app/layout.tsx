dea52d60feb092557f2aba4cc0127c79a0f083b3
Kailana Sommer <kailana.work@gmail.com>
2025-08-03 18:43:52 +1000

    update png

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,53 +1,20 @@
-"use client";
+import { montserrat, bricolage } from '@/lib/fonts'
+import RootLayout from '@/components/RootLayout'
+import './globals.css'
 
-import React from "react";
-import { montserrat, bricolage } from './fonts'
-import { AuthProvider } from "@/providers/AuthProvider";
-import { LayoutProps } from "@/types/layout";
-import "./globals.css";
-import MenuItem from "@/components/common/MenuItem";
-import { Routes } from "@/enums/routes";
-import UserSession from "@/components/common/UserSession";
-import Link from "next/link";
-import { ProfileProvider } from "@/providers/ProfileProvider";
-import GrassIcon from '@mui/icons-material/Grass';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-const queryClient = new QueryClient()
-
-
-const Layout = ({ children }: LayoutProps) => {
-    return (
-        <html lang="en">
-            <body className={`${montserrat.variable} ${bricolage.variable}`}>
-                <QueryClientProvider client={queryClient}>
-                    <AuthProvider>
-                        <ProfileProvider>
-                            <header className="flex items-center justify-between mx-2 mt-4  card-alt">
-                                <div className="min-w-40">
-                                    <Link href="/">
-                                        <h1>
-                                            Slo
-                                            <GrassIcon className="mt-2"/>
-                                            Id
-                                        </h1>
-                                    </Link>
-                                </div>
-                                <nav>
-                                    <MenuItem route={Routes.MAP} item="Map" />
-                                    <MenuItem route={Routes.PROFILE} item="Profile" />
-                                    <UserSession/> 
-                                </nav>
-                            </header>
-                            <main className="px-2">  
-                                {children}
-                            </main>
-                        </ProfileProvider>
-                    </AuthProvider>
-                </QueryClientProvider>
-            </body>
-        </html>
-    );
+export const metadata = {
+  title: 'Slo-id',
+  description:
+    'Slo-id focuses on identification in depth, avoiding misidentifications by examining key details, and tracking sightings to understand how it all fits together.',
+  icons: {
+    icon: '/slo-id.png',
+  },
 };
 
-export default Layout;
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" className={`${montserrat.variable} ${bricolage.variable}`}>
+      <RootLayout>{children}</RootLayout>
+    </html>
+  );
+}
---
+++ src/components/RootLayout.tsx
@@ -0,0 +1,53 @@
+"use client";
+import React from "react";
+import { montserrat, bricolage } from '@/lib/fonts'
+import { AuthProvider } from "@/providers/AuthProvider";
+import { LayoutProps } from "@/types/layout";
+import MenuItem from "@/components/common/MenuItem";
+import { Routes } from "@/enums/routes";
+import UserSession from "@/components/common/UserSession";
+import Link from "next/link";
+import { ProfileProvider } from "@/providers/ProfileProvider";
+// import GrassIcon from '@mui/icons-material/Grass';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Image from 'next/image';
+
+const queryClient = new QueryClient();
+
+const RootLayout = ({ children }: LayoutProps) => {
+  return (
+    <body className={`${montserrat.variable} ${bricolage.variable}`}>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <ProfileProvider>
+            <header className="flex items-center justify-between mx-2 mt-4 card-alt">
+            <div className="min-w-40">
+            <Link href="/">
+              <h1 className="flex items-center gap-1">
+                Slo
+                {/* <GrassIcon className="mt-[2px]" /> */}
+                <Image
+                  src="/slo-id.png"
+                  alt="Slo id logo"
+                  width={24}
+                  height={24}
+                />
+                Id
+              </h1>
+            </Link>
+          </div>
+              <nav>
+                <MenuItem route={Routes.MAP} item="Map" />
+                <MenuItem route={Routes.PROFILE} item="Profile" />
+                <UserSession />
+              </nav>
+            </header>
+            <main className="px-2">{children}</main>
+          </ProfileProvider>
+        </AuthProvider>
+      </QueryClientProvider>
+    </body>
+  );
+};
+
+export default RootLayout;
