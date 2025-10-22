359f538e566408f2e97efb4b61d5889ecc1e66f3
K-so <72859994+kailana-so@users.noreply.github.com>
2025-10-23 07:16:42 +1100

    Delete server/dist directory

--- server/dist/middleware/systemKey.js
+++
@@ -1,8 +0,0 @@
-export const systemKeyMiddleware = (req, res, next) => {
-    const systemKey = req.headers['x-system-key'];
-    const validSystemKey = process.env.NEXT_PUBLIC_X_SYSTEM_KEY;
-    if (systemKey !== validSystemKey) {
-        return res.status(401).json({ error: 'Unauthorized' });
-    }
-    next();
-};
