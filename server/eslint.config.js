b37de4bf0f2a524274e4667ef6359fc4cd8961ba
Kailana Sommer <kailana.work@gmail.com>
2025-10-22 20:48:32 +1100

    fixing packages

---
+++ server/eslint.config.js
@@ -0,0 +1,16 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+    rules: {
+      // Add any custom rules here
+    },
+  }
+);
